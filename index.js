df5eb469fbd8671d0f0c9f58286229e8e76ea7c4
halimbagusalhakim <halsat132@gmail.com>
2025-10-27 09:23:13 +0700

    konfigurasi middleware

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const PORT = 3000;
 const db = require('./models');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.listen(PORT), () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+}
 
