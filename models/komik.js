1bc4dddab89370cec304977916cb4bbbc3ee5555
halimbagusalhakim <halsat132@gmail.com>
2025-10-27 09:32:17 +0700

    memperbaiki kesalahan kode

--- models/komik.js
+++ models/komik.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
 
     },
     description: {
-        type: DataTypes.TEXT,
+        type: DataTypes.STRING,
     },
     author: {
         type: DataTypes.STRING,
